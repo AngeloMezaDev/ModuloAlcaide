ec88a3803b64a6c1abd6589149fbf6c3b0a92895
Yordan <Yordan@YordanVC>
2023-05-24 16:38:38 -0500

    cambio de size del frame

--- src/proyectocs/frmAlcaide.java
+++ src/proyectocs/frmAlcaide.java
@@ -32,7 +32,7 @@ public class frmAlcaide extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 801, Short.MAX_VALUE)
+            .addGap(0, 900, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
