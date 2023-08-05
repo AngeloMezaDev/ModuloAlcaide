a93d176b5203b5928df4bf23abed4a5190f3b00f
RickV777 <134444002+RickV777@users.noreply.github.com>
2023-08-04 21:04:42 -0500

    Update Proyecto_CS.sql
    Actualización

--- Proyecto_CS.sql
+++ Proyecto_CS.sql
@@ -705,6 +705,7 @@ END;
 /
 
 -- Creacion de tabla Inscripcion
+Drop table Inscripcion;
 CREATE TABLE Inscripcion (
   ID_Inscripcion VARCHAR2(6 BYTE),
   Id_Recluso VARCHAR2(6 BYTE),
@@ -786,7 +787,8 @@ BEGIN
 END;
 /
 
---MODULO PROFESOR
+--MODULO PROFESOR}
+Drop table Asistencias
 CREATE TABLE Asistencias (
     Id_asistencia VARCHAR(20) PRIMARY KEY,
     Id_recluso VARCHAR(20),
